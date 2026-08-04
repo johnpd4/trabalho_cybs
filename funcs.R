bd71264f1d81b96019930bd2bd099a6712408e08
johnpd4 <johnpd4@DESKTOP-johnpd4.john.lar.art.br>
2026-08-04 15:56:38 -0300

    bdp funcional (yippie) + a_j e b_j

--- funcs.R
+++ funcs.R
@@ -1,4 +1,4 @@
-BDP = function(a, t_max, lambda_k, mu_k, theta){
+BDP_base = function(a, t_max, theta_update, theta){
 
   k = a
   tau = 0
@@ -12,29 +12,28 @@ BDP = function(a, t_max, lambda_k, mu_k, theta){
 
   while(sum(path$T.) <= t_max){
 
-    prob_birth = lambda_k(k, theta)
-    prob_death = mu_k(k, theta)
+    rates = theta_update(k, theta)
+    rate_birth = rates[1]
+    rate_death = rates[2]
     
-    tau = rexp(1, rate = (prob_birth + prob_death))
+    tau = rexp(1, rate = (rate_birth + rate_death))
     
-    if(sum(path$T.) + tau >= t_max){
+    if(sum(path$T.) + tau > t_max){
       path[nrow(path)+1, ] = c(k, 0, 0, t_max - sum(path$T.))
       return(path)
     }
 
-    birth_range = prob_birth
-    death_range = prob_birth + prob_death
+    birth_range = rate_birth / (rate_birth + rate_death)
+    # death_range = prob_birth + prob_death
 
     random = runif(1)
 
     if(random <= birth_range){
       k = k + 1
       path[nrow(path)+1, ] = c(k, 1, 0, tau)
-    } else if(random <= death_range){
+    } else {
       k = k - 1
       path[nrow(path)+1, ] = c(k, 0, 1, tau)
-    } else {
-      path[nrow(path)+1, ] = c(k, 0, 0, tau)
     }
 
   }
@@ -43,25 +42,86 @@ BDP = function(a, t_max, lambda_k, mu_k, theta){
 
 }
 
-lambda_k_simple_linear = function(k, theta){
+BDP = function(a, t_max, method, theta){
   
-  if(length(theta) > 2){stop("Length of theta implies immigration, but birth function for simple linear was used!")}
+  # METHODS:
+  # linear -> theta = (\gamma, \mu)
+  # immigration -> theta = (\gamma, \mu, \nu)
+  # restricted_growth -> theta = (\gamma, \mu, \beta)
+  # sis -> theta = (\beta, \gamma, N)
+  
+  func = switch(method,
+    "linear" = theta_update_simple_linear,
+    "immigration" = theta_update_immigration,
+    "restricted_growth" = theta_update_restricted_growth,
+    "sis" = theta_update_sis,
+    stop("Unknown method: ", method)
+  )
+  
+  obj = BDP_base(a = a, t_max = t_max,
+                 theta_update = func,
+                 theta = theta)
+  
+  return(obj)
+  
+}
 
-  return(min(1, k*theta[1]))
+a_j = function(j, theta_update, theta){
+  
+  if(j == 1){return(1)}
+  
+  gamma = theta_update_func(j - 2, theta) |> getElement(1)
+  mu = theta_update_func(j - 1, theta) |> getElement(2)
+  
+  return(-gamma * mu)
+  
+}
+
+b_j = function(s, j, theta_update, theta){
+  
+  if(j == 1){
+    gamma0 = theta_update_func(0, theta) |> getElement(1)
+    return(s + gamma0)
+  }
+  
+  params = theta_update_func(j - 1, theta)
+  gamma = params[1]
+  mu = params[2]
+  
+  return(s + gamma + mu)
+  
+}
+
+theta_update_simple_linear = function(k, theta){
+  
+  # if(length(theta) > 2){stop("Length of theta implies immigration, but birth function for simple linear was used!")}
+
+  return(c(k*theta[1], k*theta[2]))
 
 }
 
-mu_k_simple_linear = function(k, theta){
-
-  return(min(1, k*theta[2]))
-
+theta_update_immigration = function(k, theta){
+  
+  # if(length(theta) < 3){stop("Length of theta implies no immigration, but birth function for immigration was used!")}
+  
+  return(c(k*theta[1] + theta[3], k*theta[2]))
+  
 }
 
-lambda_k_linear_immigration = function(k, theta){
+theta_update_restricted_growth = function(k, theta){
   
-  if(length(theta) < 3){stop("Length of theta implies no immigration, but birth function for immigration was used!")}
+  # if(length(theta) < 3){stop("Length of theta implies no immigration, but birth function for immigration was used!")}
   
-  return(min(1, k*theta[1] + theta[3]))
+  return(c(theta[1] * k^2 * exp(-theta[3] * k), k*theta[2]))
+  
+}
+
+theta_update_sis = function(k, theta){
+  
+  # if(length(theta) < 3){stop("Length of theta implies no immigration, but birth function for immigration was used!")}
+  
+  return(c(theta[1] * k * (theta[3] - k) / theta[3],
+           k * theta[2] / theta[3]))
   
 }
 
